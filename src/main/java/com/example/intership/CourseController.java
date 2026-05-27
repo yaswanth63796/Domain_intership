56b4e20d88265812b02ac27b07243b34ca2536d5
yaswanth63796 <yeshuv24@gmail.com>
2026-05-27 10:44:03 +0530

    made some chnages

--- src/main/java/com/example/intership/CourseController.java
+++ src/main/java/com/example/intership/CourseController.java
@@ -1,6 +1,7 @@
 package com.example.intership;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import static org.apache.logging.log4j.util.StringBuilders.equalsIgnoreCase;
 
 
 @RestController
+@RequestMapping("/courses")
 public class CourseController {
 
  static  List<Course>list=new ArrayList<>();
@@ -21,4 +23,50 @@ public class CourseController {
         list.add(new Course("104","tamil",5));
     }
 
+@GetMapping()
+    public ResponseEntity<List<Course>>getallcourses(){
+         return ResponseEntity.ok(list);
+}
+
+@GetMapping("/getcourse/{code}")
+public ResponseEntity<Course>getcourse(@PathVariable String code){
+
+
+        for(Course C:list){
+            if(C.getCoursecode().equals(code)){
+                return ResponseEntity.ok(C);
+            }
+        }
+        return ResponseEntity.notFound().build();
+}
+@PostMapping("/add")
+    public ResponseEntity<Course>addCourse(@RequestBody Course course){
+        list.add(course);
+        return ResponseEntity.ok(course);
+}
+@DeleteMapping("/delete/{code}")
+    public ResponseEntity<Course>deletecourses(@PathVariable String  code){
+
+        for(Course C:list){
+            if(C.getCoursecode().equals(code)){
+                list.remove(C);
+                return ResponseEntity.ok(C);
+            }
+        }
+ return ResponseEntity.notFound().build();
+}
+
+@PutMapping("/update/{code}")
+    public ResponseEntity<Course>updatecourse(@PathVariable String code,@RequestBody Course updatecourse){
+
+        for(Course c:list){
+            if(c.getCoursecode().equals(code)){
+                c.setCourseName(updatecourse.getCourseName());
+                c.setCredits((updatecourse.getCredits()));
+                return ResponseEntity.ok(c);
+            }
+
+        }
+        return ResponseEntity.notFound().build();
+}
 }
