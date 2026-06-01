656350c40dc888c5d9251de9fc62bb441ed0ec1d
yaswanth63796 <yeshuv24@gmail.com>
2026-06-01 09:49:53 +0530

    done a getmapping  using controller and service

--- src/main/java/com/example/practice_2/crud/operations/Controller/StudentController.java
+++ src/main/java/com/example/practice_2/crud/operations/Controller/StudentController.java
@@ -1,5 +1,25 @@
 package com.example.practice_2.crud.operations.Controller;
 
+
+import com.example.practice_2.crud.operations.Service.StudentService;
+import com.example.practice_2.crud.operations.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+        @Autowired
+       StudentService s;
+
+
+        @GetMapping("students")
+    public List<Student> getallstudents(){
+        return s.getallstudents();
+    }
+
 
 }
---
+++ src/main/java/com/example/practice_2/crud/operations/Service/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.practice_2.crud.operations.Service;
+
+import com.example.practice_2.crud.operations.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+    List<Student>students= Arrays.asList(new Student(1,"yaswanth","CSE"),new Student(2,"srinath","CSE"));
+    public List<Student> getallstudents() {
+         return students;
+    }
+}
