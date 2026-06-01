952a332aed6c633ab666ad09c582a0a194ddaa4e
yaswanth63796 <yeshuv24@gmail.com>
2026-06-01 10:06:43 +0530

    done getmapping and postmapping

--- src/main/java/com/example/practice_2/crud/operations/Controller/StudentController.java
+++ src/main/java/com/example/practice_2/crud/operations/Controller/StudentController.java
@@ -4,22 +4,26 @@ package com.example.practice_2.crud.operations.Controller;
 import com.example.practice_2.crud.operations.Service.StudentService;
 import com.example.practice_2.crud.operations.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("Student")
 public class StudentController {
 
         @Autowired
        StudentService s;
 
 
-        @GetMapping("students")
+        @GetMapping("/get")
     public List<Student> getallstudents(){
         return s.getallstudents();
     }
 
+    @PostMapping("/add")
+     public Student addStudent(@RequestBody Student student){
+            return s.addStudent(student);
+    }
 
 }
--- src/main/java/com/example/practice_2/crud/operations/Service/StudentService.java
+++ src/main/java/com/example/practice_2/crud/operations/Service/StudentService.java
@@ -4,6 +4,7 @@ import com.example.practice_2.crud.operations.model.Student;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,5 +15,12 @@ public class StudentService {
     List<Student>students= Arrays.asList(new Student(1,"yaswanth","CSE"),new Student(2,"srinath","CSE"));
     public List<Student> getallstudents() {
          return students;
+    }
+
+    public Student addStudent(Student student) {
+        students.add(student);
+        return student;
+
+
     }
 }
